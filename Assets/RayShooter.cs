89c443df10b5ed39896823beef3e27ef5e7f08a8
Rahul Prasanna <rahul.srivatsav1995@gmail.com>
2018-01-18 14:35:18 +0530

    added enemy movement

--- Assets/RayShooter.cs
+++ Assets/RayShooter.cs
@@ -26,7 +26,13 @@ public class RayShooter : MonoBehaviour {
 			Ray ray = _camera.ScreenPointToRay(point);
 			RaycastHit hit;
 			if(Physics.Raycast(ray, out hit)){
-				StartCoroutine(SphereIndicator(hit.point));
+				GameObject hitObject = hit.transform.gameObject;
+				ReactiveTarget target = hitObject.GetComponent<ReactiveTarget> ();
+				if (target != null) {
+					target.ReactToHit ();
+				} else {
+					StartCoroutine(SphereIndicator(hit.point));	
+				}
 			}
 		}
 	}
