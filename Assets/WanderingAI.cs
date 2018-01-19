b777be782c492f66719d716baa622bafca1bc9a0
Rahul Prasanna <rahul.srivatsav1995@gmail.com>
2018-01-19 13:48:28 +0530

    added enemy shooting and textures for walls and floors

--- Assets/WanderingAI.cs
+++ Assets/WanderingAI.cs
@@ -7,6 +7,9 @@ public class WanderingAI : MonoBehaviour {
 	public float obstacleRange = 5.0f;
 	private bool _alive;
 
+	[SerializeField] private GameObject fireballPrefab;
+	private GameObject _fireball;
+
 	// Use this for initialization
 	void Start () {
 		_alive = true;
@@ -20,7 +23,15 @@ public class WanderingAI : MonoBehaviour {
 			Ray ray = new Ray (transform.position, transform.forward);
 			RaycastHit hit;
 			if (Physics.SphereCast (ray, 0.75f, out hit)) {
-				if (hit.distance < obstacleRange) {
+				GameObject hitObject = hit.transform.gameObject;
+				if (hitObject.GetComponent<PlayerCharacter> ()) {
+					if (_fireball == null) {
+						_fireball = Instantiate (fireballPrefab) as GameObject;
+						_fireball.transform.position = transform.TransformPoint (Vector3.forward * 1.5f);
+						_fireball.transform.rotation = transform.rotation;
+					}
+				}
+				else if (hit.distance < obstacleRange) {
 					float angle = Random.Range (-110, 110);
 					transform.Rotate (0, angle, 0);
 				}
